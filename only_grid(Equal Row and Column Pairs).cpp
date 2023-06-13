d91524b6184ac50d7474d98f9b07bba159993286
@coder shruti <74962507+ShrutiRani7029@users.noreply.github.com>
2023-06-13 18:32:16 +0530

    Create only grid(Equal Row and Column Pairs).cpp
    Time Complexity: o(n^2)
    Space Complexity: o(n)

---
+++ only_grid(Equal Row and Column Pairs).cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int equalPairs(vector<vector<int>>& grid) {
+        //r2,c2   r3,c2    r2,c1
+        int n=grid.size();
+        int m=grid[0].size();
+        int ans=0;
+        
+        map<vector<int>,int>mp;
+        for(int i=0;i<n;i++){
+            mp[grid[i]]++; //row addition
+        }
+        
+        for(int j=0;j<m;j++){
+            vector<int>v; 
+            for(int i=0;i<n;i++){
+                v.push_back(grid[i][j]);
+            }
+            ans+=mp[v];
+        }
+        return ans;
+        
+        
+    }
+};
